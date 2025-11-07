24e3fc2a6209ec5b65a768a5f0618236e44c4e5d
LazySeliverstov <167297964+LazySeliverstov@users.noreply.github.com>
2025-11-07 21:01:45 +0300

    add gorutine and log logick

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module otus
 
 go 1.21.4
 
-require github.com/google/uuid v1.6.0 // indirect
+require github.com/google/uuid v1.6.0
--- service/generator.go
+++ service/generator.go
@@ -3,17 +3,19 @@ package service
 import (
 	"math/rand"
 	"otus/models"
+	"sync"
 
 	"github.com/google/uuid"
 )
 
-func GenerateModels() models.EatType {
+func GenerateModels(EatTypeChannel chan models.EatType, wg *sync.WaitGroup) {
+	defer wg.Done()
 	switch i := rand.Intn(3); i {
 	case 0:
-		return models.Dish{Id: rand.Intn(9999)}
+		EatTypeChannel <- models.Dish{Id: rand.Intn(9999)}
 	case 1:
-		return models.Menu{Id: uuid.New()}
+		EatTypeChannel <- models.Menu{Id: uuid.New()}
 	default:
-		return models.Restaurant{Id: uuid.New()}
+		EatTypeChannel <- models.Restaurant{Id: uuid.New()}
 	}
 }
