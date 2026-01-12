edab2fc923e0d871f2104734c483e3edea136b81
LazySeliverstov <167297964+LazySeliverstov@users.noreply.github.com>
2026-01-12 23:51:04 +0300

    add work with json
    Добавил чтение из файла и сохранение в него. Сейчас правда открывается и перезаписывается весь файл целиком.

--- internal/models/safeSlice.go
+++ internal/models/safeSlice.go
@@ -1,36 +1,75 @@
 package models
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 	"sync"
 )
 
-type SafeSlice[slice EatType] struct {
+type SafeSlice[T EatType] struct {
 	mu    sync.Mutex
-	slice []slice
+	slice []T
+	File  string
 }
 
-func (safeSl *SafeSlice[EatType]) Append(eat EatType) {
+func (safeSl *SafeSlice[T]) Append(eat T) error {
 	safeSl.mu.Lock()
 	defer safeSl.mu.Unlock()
 	safeSl.slice = append(safeSl.slice, eat)
+	return safeSl.saveToFile()
 }
 
-func (safeSl *SafeSlice[EatType]) Length() int {
+func (safeSl *SafeSlice[T]) saveToFile() error {
+	file, err := os.Create(safeSl.File)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+	return encoder.Encode(safeSl.slice)
+}
+
+func (safeSl *SafeSlice[T]) Length() int {
 	safeSl.mu.Lock()
 	defer safeSl.mu.Unlock()
 	return len(safeSl.slice)
 }
 
-func (safeSl *SafeSlice[EatType]) ReturnElement(i int) EatType {
+func (safeSl *SafeSlice[T]) ReturnElement(i int) EatType {
 	return safeSl.slice[i]
 }
 
-func (safeSl *SafeSlice[EatType]) LogNewElements(state *int, printertype string) {
+func (safeSl *SafeSlice[T]) LogNewElements(state *int, printertype string) {
 	if *state < safeSl.Length() {
 		for i := *state; i < safeSl.Length(); i++ {
 			log.Println("Новый тип", printertype, safeSl.slice[i])
 		}
 		*state = safeSl.Length()
 	}
+}
+
+func (safeSl *SafeSlice[T]) LoadFromFile() error {
+	safeSl.mu.Lock()
+	defer safeSl.mu.Unlock()
+
+	file, err := os.Open(safeSl.File)
+	if err != nil {
+		if os.IsNotExist(err) { // файла нет — просто оставляем slice пустым
+			safeSl.slice = []T{}
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+
+	var data []T
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&data); err != nil {
+		return err
+	}
+
+	safeSl.slice = data
+	return nil
 }
--- internal/repository/filtrarion.go
+++ internal/repository/filtrarion.go
@@ -4,9 +4,21 @@ import (
 	"github.com/failfilin/otus_hw/internal/models"
 )
 
-var RestSlice models.SafeSlice[models.Restaurant]
-var MenuSlice models.SafeSlice[models.Menu]
-var DishSlice models.SafeSlice[models.Dish]
+var RestSlice = models.SafeSlice[models.Restaurant]{
+	File: "data/restaurant.json",
+}
+var MenuSlice = models.SafeSlice[models.Menu]{
+	File: "data/menu.json",
+}
+var DishSlice = models.SafeSlice[models.Dish]{
+	File: "data/dishes.json",
+}
+
+func InitRepository() {
+	_ = RestSlice.LoadFromFile()
+	_ = MenuSlice.LoadFromFile()
+	_ = DishSlice.LoadFromFile()
+}
 
 func AddSlice(i models.EatType) {
 	switch v := i.(type) {
